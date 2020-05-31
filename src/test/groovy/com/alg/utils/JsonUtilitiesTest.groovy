f7177f0a03c1e198eb83c60dc05e723e970afb10
ArturoLG <lgzarturo@gmail.com>
2020-05-31 03:00:00 -0500

    Corrección de pruebas para el mapa en json

--- src/test/groovy/com/alg/utils/JsonUtilitiesTest.groovy
+++ src/test/groovy/com/alg/utils/JsonUtilitiesTest.groovy
@@ -4,14 +4,14 @@ import org.junit.Test
 
 class JsonUtilitiesTest extends GroovyTestCase {
 
-    final def JSON = '{"person":{"name":"Arturo López","age":37,"pets":["dog","cat"]}}'
+    final def JSON = '{"name":"Arturo López","age":37,"pets":["dog","cat"]}'
     Map<String, String> MAP = new HashMap<String, String>();
 
     @Test
     void testGetJsonMap() {
-        MAP.put("username", "lgzarturo")
-        MAP.put("email", "lgzarturo@gmail.com")
         def map = JsonUtilities.getJsonMap(JSON)
-        assertEquals(MAP, map)
+        assertEquals("Arturo López", map.name)
+        assertEquals(37, map.age)
+        assertEquals(["dog","cat"], map.pets)
     }
 }
